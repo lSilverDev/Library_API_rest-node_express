47c51fe1fb07426061a8c61b19a799322a8b239a
Eduardo <67278780+lSilverDev@users.noreply.github.com>
2022-11-10 16:08:15 -0300

    Delete dbConnect.js

--- src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://admin:1m4dp2MTYFGOzyhZ@cluster0.nrzqmq2.mongodb.net/Cluster0?retryWrites=true&w=majority");
-
-let db = mongoose.connection;
-
-export default db;

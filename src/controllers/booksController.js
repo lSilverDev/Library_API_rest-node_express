f3a59d83c4bce87fd41519c38fac6557fe8503ca
Eduardo <67278780+lSilverDev@users.noreply.github.com>
2022-11-11 14:19:54 -0300

    Crud and project pattern

---
+++ src/controllers/booksController.js
@@ -0,0 +1,78 @@
+import { response } from "express";
+import books from "../models/Book.js";
+
+class BookController{
+
+    static getAllBooks = (request, response) => {
+        books.find((err, books) => {
+            response.status(200).json(books);
+        })
+    }
+
+    static getBookByID = (request, response) => {
+        const id = request.params.id;
+
+        books.findById(id, (err, books) => {
+            if(!err){
+                response.status(200).send(books);
+            } else {
+                response.status(400).send({
+                    message: "Error: " + err.message + " book not found",
+                });
+            }
+        })
+    }
+
+    static setBook = (request, response) => {
+        let book = new books(request.body);
+
+        book.save((err) => {
+            if(err){
+                response.status(500).send({
+                    message: "Error:" + err.message
+                });
+            } else {
+                response.status(201).send(book.toJSON());
+            }
+        });
+    }
+
+    static updateBookTitle = (request, response) => {
+        const id = request.params.id;
+
+        books.findByIdAndUpdate(id, {
+            $set: request.body
+        }, (err) => {
+            if(!err){
+                response.status(200).send({
+                    message: "Book updated"
+                });
+            } else {
+                response.status(500).send({
+                    message: "Error:" + err.message,
+                });
+            }
+        })
+    }
+
+    static deleteBookByID = (request, response) => {
+        const id = request.params.id;
+
+        books.findByIdAndDelete(id, 
+            (err) => {
+                if(!err){
+                    response.status(200).send({
+                        message: "book deleted",
+                    });
+                } else {
+                    response.status(500).send({
+                        message: "Error:" + err.message,
+                    });
+                }
+            }
+        )
+    }
+    
+}
+
+export default BookController;
---
+++ src/routes/booksRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import booksController from "../controllers/booksController.js"
+
+const router = express.Router();
+
+router
+    .get("/books", booksController.getAllBooks)
+    .get("/books/:id", booksController.getBookByID)
+    .post("/books", booksController.setBook)
+    .put("/books/:id", booksController.updateBookTitle)
+    .delete("/books/:id", booksController.deleteBookByID)
+
+export default router;
